e93d795349bc54cd2c6dcc543b3521b38a2129e3
AsmitaKharat17 <130168619+AsmitaKharat17@users.noreply.github.com>
2023-05-01 20:11:00 +0530

    Update CodeOptimization.py

--- CodeOptimization.py
+++ CodeOptimization.py
@@ -1,5 +1,8 @@
+# Code Optimisation
+# 1. Constant Propagation
+# 2. Dead Code Elimination
 import re
-import ast
+
 
 def input_code():
     code = []
@@ -7,6 +10,7 @@ def input_code():
     for _ in range(n):
         code.append(input())
     return code
+
 
 def const_prop(code):
     const = {}
@@ -19,41 +23,42 @@ def const_prop(code):
         else:
             r = r.split(" ")
             opt.append([l, r])
-        
-        
     for line in opt:
         for k in const.keys():
             if(k in line[1]):
                 line[1] = [const[k] if x == k else x for x in line[1]]
-        
     for i, line in enumerate(opt):
         opt[i] = line[0] + " = " + " ".join(line[1])
     return opt
 
+
 def dead_code(code):
-    const = {}
-    opt = []
-    flag1 = 0
-    flag2 = 0
-    for line in code:
-        if("{" in line):
-            flag2 += 1
-        elif("}" in line):
-            flag2 -= 0
-        elif(flag1 == 1 and flag2 > 0):
-            opt.append(line)
-        elif("if" in line):
-            cond = re.findall("(?<=\().+?(?=\))", line)
-            new_cond = [const[x.strip()] if (
-                x.strip() in const.keys()) else x for x in cond[0].split(" ")]
-            a, b = new_cond[0], new_cond[2]
-            if(a == b):
-                flag1 = 1
-        else:
-            opt.append(line)
-            l, r = map(lambda x: x.strip(), line.split("="))
-        const[l] = r[0]
-    return opt
+	const = {}
+	opt = []
+	flag1 = 0
+	flag2 = 0
+	for line in code:
+		if("{" in line):
+			flag2 += 1
+		elif("}" in line):
+			flag2 -= 1
+		elif(flag1 == 1 and flag2 > 0):
+			opt.append(line)
+		elif("if" in line):
+			cond = re.findall("(?<=\().+?(?=\))", line)
+			new_cond = [const[x.strip()] if (
+				x.strip() in const.keys()) else x for x in cond[0].split(" ")]
+			a, b = new_cond[0], new_cond[2]
+			if(a == b):
+				flag1 = 1
+			else:
+				flag1 = 0
+		else:
+			if flag2 == 0 or flag1 == 1:
+				opt.append(line)
+				l, r = map(lambda x: x.strip(), line.split("="))
+				const[l] = r[0]
+	return opt
 
 
 def display_code(opt):
@@ -61,12 +66,13 @@ def display_code(opt):
     for line in opt:
         print(line)
 
+
 while True:
     print("\n***************************************")
     print("Choose Code Optimization Technique")
     print("1. Constant Propagation")
     print("2. Dead Code Elimination")
-    ch = int(input("\nEnter choice: "))
+    ch = int(input("Enter choice: "))
     if(ch == 1):
         input_prog = input_code()
         output_prog = const_prop(input_prog)
@@ -78,3 +84,7 @@ while True:
     else:
         print("Bye")
         break
+        
+        
+        
+ 
